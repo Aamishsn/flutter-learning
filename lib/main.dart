e64cd5d53c33febb8e405b6e31cb5d43e56f4204
Aamishsn <aamishsn@gmail.com>
2024-10-13 18:33:33 +0500

    text widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const Text(
-      "Hello World",
+      "Hello World!!!!",
       textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 20,
+        color: Colors.red,
+        fontWeight: FontWeight.bold
+      )
     ),
   );
   // runApp(const MyApp());
