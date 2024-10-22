d6bab68de50122e6d9ddcf6a0469a297cb9e4664
Aamishsn <aamishsn@gmail.com>
2024-10-22 21:22:51 +0500

    221024

---
+++ lib/currency_convertor.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConvertor extends StatelessWidget {
+  const CurrencyConvertor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        home: Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment : MainAxisAlignment.center,
+          children: [
+              Text("Hello World amish hasan"),
+          ],
+        ))
+    ));
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
+import 'package:currency_converter/currency_convertor.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const Text(
-      "Hello World!!!!",
-      textDirection: TextDirection.ltr,
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        fontSize: 20,
-        color: Colors.red,
-        fontWeight: FontWeight.bold
-      )
-    ),
-  );
+  runApp(const MyApp());
   // runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        home: CurrencyConvertor());
+  }
 }
